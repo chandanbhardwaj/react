f5ac217ad75c10a6b45dae5a7a3603d2ac153403
chandanbhardwaj <ckbbarh@gmail.com>
2020-10-20 00:13:11 +0530

    Updates

---
+++ precache-manifest.76285a2a318f957bcf5ad29329b218b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4700891ef56037a3daf5a86279053737",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "103393c6119be433db3f",
+    "url": "/react/static/css/main.9d10fb6b.chunk.css"
+  },
+  {
+    "revision": "ba7c17bae35ff1d449bd",
+    "url": "/react/static/js/2.8835a34d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react/static/js/2.8835a34d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103393c6119be433db3f",
+    "url": "/react/static/js/main.7e81e178.chunk.js"
+  },
+  {
+    "revision": "5436e33bbc49246e765c",
+    "url": "/react/static/js/runtime-main.2be02786.js"
+  }
+]);
